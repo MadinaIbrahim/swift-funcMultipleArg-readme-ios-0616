a3c050b90d0f1000bf0434d5f2b4543d37d92a25
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-07-06 02:56:50 -0400

    Done.

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -1,8 +1,8 @@
-func moonsOfJupiter() {
-    print("There are 67 moons orbiting Jupiter.")
+func moonsOfJupiter(planet: String, numberOfMoons: Int) {  //// function definition
+    print("There are \(numberOfMoons) moons orbiting \(planet).")
 }
 
-moonsOfJupiter()
+//moonsOfJupiter()
 
 
 // planetDescription, take 1
@@ -31,13 +31,41 @@ func planetDescription(planet: String, numberOfMoons: Int) {
 
 // This line of code gives an error.
 // Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
+////planetDescription("Jupiter", 67) // will give an error
+//When calling a function with multiple arguments, you have to specify the name of the argument
+//for the second (and subsequent) arguments, 
+//using the same name specified in the function definition.
 
 planetDescription("Jupiter", numberOfMoons: 67)
 
 var planet = "Jupiter"
 var moons = 67
 planetDescription(planet, numberOfMoons: moons)
+
 planet = "Mars"
 moons = 2
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Earth"
+moons = 1
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Mercury"
+moons = 0
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Venus"
+moons = 0
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Saturn"
+moons = 62
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Neptune"
+moons = 14
+planetDescription(planet, numberOfMoons: moons)
+
+planet = "Uranus"
+moons = 27
 planetDescription(planet, numberOfMoons: moons)
